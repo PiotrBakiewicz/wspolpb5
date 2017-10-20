52522741b952cb53642f5f8d1901d71856be05a4
PiotrBakiewicz <mojave@onet.eu>
2017-10-20 11:24:58 +0200

    notaki

---
+++ pierwszyprogram.c
@@ -0,0 +1,16 @@
+# include <stdio.h> /*biblioteka standardowa*/
+/* y=f(x)*/
+/*   f() => 0*/
+int main()
+{
+puts("pierwszyprogram");
+
+
+putchar('g');
+putchar('\n');
+putchar('\t');
+putchar('\r');
+putchar('\\');
+return 0;
+}
+
